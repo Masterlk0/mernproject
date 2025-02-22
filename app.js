4af4f4598bfd52b35d63e41230bcbd422f81a26c
Tharindu Rathnayake <165813483+Masterlk0@users.noreply.github.com>
2025-02-22 14:50:42 +0530

    mongodb connected

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 console.log("bye");
+// ZpsIJs1tixGuX6BK,dNsGumiYKymY5dLu
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+// Middleware and Routes
+app.get("/", (req, res) => {
+    res.send("It is working");
+});
+
+// Connect to MongoDB and start the server
+mongoose.connect("mongodb+srv://admin1:dNsGumiYKymY5dLu@cluster0.mdvv7.mongodb.net/", { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+})
+.then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(5000, () => {
+        console.log("Server is running on http://localhost:5000");
+    });
+})
+.catch((err) => console.log(err));
